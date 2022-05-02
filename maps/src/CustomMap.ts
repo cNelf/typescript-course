1e92d2cf7b0712136ece0519d84d184e84891da8
cNelf <1184974829@qq.com>
2022-05-02 11:08:38 +0800

    feat: finish maps

--- maps/src/CustomMap.ts
+++ maps/src/CustomMap.ts
@@ -1,8 +1,10 @@
-interface Mappable {
+export interface Mappable {
   location: {
     lat: number;
     lng: number;
   };
+  markerContent(): string;
+  color: string;
 }
 
 export class CustomMap {
@@ -19,12 +21,18 @@ export class CustomMap {
   }
 
   addMarker(mappable: Mappable): void {
-    new google.maps.Marker({
+    const marker = new google.maps.Marker({
       map: this.googleMap,
       position: {
         lat: mappable.location.lat,
         lng: mappable.location.lng,
       },
+    });
+
+    marker.addListener('click', () => {
+      new google.maps.InfoWindow({
+        content: mappable.markerContent(),
+      });
     });
   }
 }
