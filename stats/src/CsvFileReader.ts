135b4feabc5a1831319407178e97d7ad7cf3e4d7
cNelf <1184974829@qq.com>
2022-05-04 02:13:02 +0800

    fix: stats project use generic to refactor

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,31 +1,19 @@
 import fs from 'fs';
-import { MatchResult } from './MatchResult';
-import { dateStringToDate } from './utils';
 
-type MatchData = [Date, string, string, number, number, MatchResult, string];
-
-export class CsvFileReader {
-  data: MatchData[] = [];
+export abstract class CsvFileReader<T> {
+  data: T[] = [];
 
   constructor(public filename: string) {}
 
-  read() {
+  abstract mapRow(row: string[]): T;
+
+  read(): void {
     this.data = fs
       .readFileSync(this.filename, {
         encoding: 'utf-8',
       })
       .split('\n')
       .map((row: string): string[] => row.split(','))
-      .map((row: string[]): MatchData => {
-        return [
-          dateStringToDate(row[0]),
-          row[1],
-          row[2],
-          parseInt(row[3]),
-          parseInt(row[4]),
-          row[5] as MatchResult,
-          row[6],
-        ];
-      });
+      .map((row: string[]): T => this.mapRow(row));
   }
 }
