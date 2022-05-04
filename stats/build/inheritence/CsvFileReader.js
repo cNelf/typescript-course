79c196cc6260341bb173c5e62da7aa5d5ebd908e
cNelf <1184974829@qq.com>
2022-05-04 20:33:42 +0800

    fix: stats project use composition class

--- stats/build/inheritence/CsvFileReader.js
+++ stats/build/inheritence/CsvFileReader.js
@@ -16,7 +16,8 @@ class CsvFileReader {
             encoding: 'utf-8',
         })
             .split('\n')
-            .map((row) => row.split(','));
+            .map((row) => row.split(','))
+            .map((row) => this.mapRow(row));
     }
 }
 exports.CsvFileReader = CsvFileReader;
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,11 +1,13 @@
+import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
 import { MatchResult } from './MatchResult';
 
-const reader = new MatchReader('football.csv');
-reader.read();
+const csvFileReader = new CsvFileReader('football.csv');
+const reader = new MatchReader(csvFileReader);
+reader.load();
 
 let manUnitedWins = 0;
-for (let match of reader.data) {
+for (let match of reader.matches) {
   if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
     manUnitedWins++;
   }
